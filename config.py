6547f0b16ad6be3d7727aa541666ad5561271905
l1l4chy <101237622+l1l4chy@users.noreply.github.com>
2022-11-26 22:33:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','lil_l4chy').split(';')
 
-static_proxy = 'http://KKDHKGYIJJJJGJYEJGGHGIYGKFEGGIREDJLJDHLH'
+static_proxy = 'http://KDDHKIYEJEJGGIYFJEGDIEYEKIKGFFRJHHCDHICJ'
 PROXY = ProxyCloud.parse(static_proxy)
 
 if PROXY:
