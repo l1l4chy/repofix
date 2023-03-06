4a3bc30f164ffeeaac598da2b258f2cecf528b15
l1l4chy <101237622+l1l4chy@users.noreply.github.com>
2023-03-06 17:00:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN = '5752454871:AAEcgcmVRwUw6vTdwotvhRzuEsFd3MU9Qcs' #Aqui va el token del bot
+BOT_TOKEN = '5248887155:AAH6UeyVSu0hCJhiKb8ec-bjoTedp1pmAA0' #Aqui va el token del bot
 API_ID =  '14169350' #Tu api id de telegram
 API_HASH = 'c0a4d9a7e42a2bf949e227bfbf59f8cf' #Tu api id de telegram
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','lil_l4chy').split(';')
 
-static_proxy = 'http://KICILKYHKJGKYGKDGHCEYDDGCIREDFLHDELD'
+static_proxy = ''
 PROXY = ProxyCloud.parse(static_proxy)
 
 if PROXY:
