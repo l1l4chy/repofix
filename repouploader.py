3fc6eaca4b248b941af586f64822a8646068ae84
l1l4chy <101237622+l1l4chy@users.noreply.github.com>
2023-03-06 16:59:09 +0100

    Update repouploader.py

--- repouploader.py
+++ repouploader.py
@@ -40,8 +40,8 @@ class CallingUpload:
                     except:pass
 
 
-async def create_session(proxy:ProxyCloud=None,username='fbdaniellee',password='laponosa'):
-    HOST = 'https://repotematico.uo.edu.cu/'
+async def create_session(proxy:ProxyCloud=None,username='dariel',password='Torriente#4578'):
+    HOST = 'https://aula.uclv.edu.cu/'
     HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0'}
     proxies = None
     if proxy:
